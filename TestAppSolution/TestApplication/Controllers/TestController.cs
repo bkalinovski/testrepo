351c24664a2de76396d5caf2e8f8ca47ef8bbac1
bkalinovski <bkalinovski@starnet.md>
2020-08-26 23:34:50 +0300

    Add Boilerplate for project

---
+++ TestAppSolution/TestApplication/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplication.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Greeting()
+        {
+            return "Hello World";
+        }
+    }
+}
